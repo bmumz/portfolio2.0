9dbc4a7178cc0b6a148f7cbf2bd3dc7c2b63513d
bmumz <brandimummery@gmail.com>
2022-01-24 22:20:26 -0500

    Add snackbar if form is submitted with errors

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
 import {
   Card,
   CardContent,
@@ -97,139 +97,157 @@ const ContactForm: FC = () => {
       >
         {({
           errors,
+          status,
           handleBlur,
           handleChange,
           handleSubmit,
           touched,
           values,
-        }): JSX.Element => (
-          <>
-            <CardContent
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-              }}
-              component={Form}
-              onSubmit={handleSubmit}
-            >
-              <Grid container>
-                <>
-                  <Grid
-                    container
-                    sx={{ display: "flex", justifyContent: "space-between" }}
-                  >
-                    <Grid
-                      item
-                      xs={12}
-                      sm={12}
-                      lg={6}
-                      sx={{
-                        paddingTop: (theme) => theme.spacing(3),
-                      }}
-                      className={classes.smallInputPadding}
-                    >
-                      <TextField
-                        id="name"
-                        label="name"
-                        variant="filled"
-                        fullWidth
-                        inputProps={{
-                          className: classes.inputColor,
-                        }}
-                        onBlur={handleBlur}
-                        onChange={handleChange}
-                        value={values.name}
-                        error={Boolean(touched.name && errors.name)}
-                        helperText={touched.name && errors.name}
-                      />
-                    </Grid>
-                    <Grid
-                      item
-                      xs={12}
-                      sm={12}
-                      lg={6}
-                      sx={{
-                        paddingTop: (theme) => theme.spacing(3),
-                      }}
-                    >
-                      <TextField
-                        id="email"
-                        label="email"
-                        variant="filled"
-                        fullWidth
-                        inputProps={{
-                          className: classes.inputColor,
-                        }}
-                        onBlur={handleBlur}
-                        onChange={handleChange}
-                        value={values.email}
-                        error={Boolean(touched.email && errors.email)}
-                        helperText={touched.email && errors.email}
-                      />
-                    </Grid>
-                  </Grid>
+          setStatus,
+        }): JSX.Element => {
+          const handleCheckForm = (event: React.FormEvent<HTMLFormElement>) => {
+            event.preventDefault();
+            setStatus("verifying");
+            handleSubmit(event);
+          };
 
-                  <Grid
-                    item
-                    xs={12}
-                    sm={12}
-                    lg={12}
-                    sx={{ paddingTop: (theme) => theme.spacing(3) }}
-                  >
-                    <TextField
-                      id="subject"
-                      label="subject"
-                      variant="filled"
-                      fullWidth
-                      inputProps={{
-                        className: classes.inputColor,
-                      }}
-                      onBlur={handleBlur}
-                      onChange={handleChange}
-                      value={values.subject}
-                      error={Boolean(touched.subject && errors.subject)}
-                      helperText={touched.subject && errors.subject}
-                    />
-                  </Grid>
-                  <Grid
-                    item
-                    xs={12}
-                    sm={12}
-                    lg={12}
-                    sx={{ padding: (theme) => theme.spacing(3, 0) }}
-                  >
-                    <TextField
-                      id="message"
-                      label="message"
-                      variant="filled"
-                      fullWidth
-                      multiline
-                      rows={4}
-                      inputProps={{
-                        className: classes.inputColor,
-                      }}
-                      onBlur={handleBlur}
-                      onChange={handleChange}
-                      value={values.message}
-                      error={Boolean(touched.message && errors.message)}
-                      helperText={touched.message && errors.message}
-                    />
-                  </Grid>
-                </>
-              </Grid>
+          useEffect(() => {
+            if (status === "verifying" && errors) {
+              enqueueSnackbar("Please review the form and fix any errors.", {
+                variant: "error",
+              });
+            }
+          }, [errors, status]);
+
+          return (
+            <>
               <CardContent
                 sx={{
                   display: "flex",
-                  justifyContent: "flex-end",
+                  flexDirection: "column",
                 }}
+                component={Form}
+                onSubmit={handleCheckForm}
               >
-                <Button variant="contained" type="submit">
-                  Send
-                </Button>
+                <Grid container>
+                  <>
+                    <Grid
+                      container
+                      sx={{ display: "flex", justifyContent: "space-between" }}
+                    >
+                      <Grid
+                        item
+                        xs={12}
+                        sm={12}
+                        lg={6}
+                        sx={{
+                          paddingTop: (theme) => theme.spacing(3),
+                        }}
+                        className={classes.smallInputPadding}
+                      >
+                        <TextField
+                          id="name"
+                          label="name"
+                          variant="filled"
+                          fullWidth
+                          inputProps={{
+                            className: classes.inputColor,
+                          }}
+                          onBlur={handleBlur}
+                          onChange={handleChange}
+                          value={values.name}
+                          error={Boolean(touched.name && errors.name)}
+                          helperText={touched.name && errors.name}
+                        />
+                      </Grid>
+                      <Grid
+                        item
+                        xs={12}
+                        sm={12}
+                        lg={6}
+                        sx={{
+                          paddingTop: (theme) => theme.spacing(3),
+                        }}
+                      >
+                        <TextField
+                          id="email"
+                          label="email"
+                          variant="filled"
+                          fullWidth
+                          inputProps={{
+                            className: classes.inputColor,
+                          }}
+                          onBlur={handleBlur}
+                          onChange={handleChange}
+                          value={values.email}
+                          error={Boolean(touched.email && errors.email)}
+                          helperText={touched.email && errors.email}
+                        />
+                      </Grid>
+                    </Grid>
+
+                    <Grid
+                      item
+                      xs={12}
+                      sm={12}
+                      lg={12}
+                      sx={{ paddingTop: (theme) => theme.spacing(3) }}
+                    >
+                      <TextField
+                        id="subject"
+                        label="subject"
+                        variant="filled"
+                        fullWidth
+                        inputProps={{
+                          className: classes.inputColor,
+                        }}
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                        value={values.subject}
+                        error={Boolean(touched.subject && errors.subject)}
+                        helperText={touched.subject && errors.subject}
+                      />
+                    </Grid>
+                    <Grid
+                      item
+                      xs={12}
+                      sm={12}
+                      lg={12}
+                      sx={{ padding: (theme) => theme.spacing(3, 0) }}
+                    >
+                      <TextField
+                        id="message"
+                        label="message"
+                        variant="filled"
+                        fullWidth
+                        multiline
+                        rows={4}
+                        inputProps={{
+                          className: classes.inputColor,
+                        }}
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                        value={values.message}
+                        error={Boolean(touched.message && errors.message)}
+                        helperText={touched.message && errors.message}
+                      />
+                    </Grid>
+                  </>
+                </Grid>
+                <CardContent
+                  sx={{
+                    display: "flex",
+                    justifyContent: "flex-end",
+                  }}
+                >
+                  <Button variant="contained" type="submit">
+                    Send
+                  </Button>
+                </CardContent>
               </CardContent>
-            </CardContent>
-          </>
-        )}
+            </>
+          );
+        }}
       </Formik>
     </Card>
   );
