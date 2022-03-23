78546a53939700ef35fea0ad0eda236faa341292
bmumz <brandimummery@gmail.com>
2022-03-23 14:03:44 -0400

    add projects route

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,7 @@
 export const pages = [
   { title: "Home", href: "/" },
   // { title: "About", href: "/" },
-  // { title: "Projects", href: "/" },
+  { title: "Projects", href: "/projects" },
   { title: "Blog", href: "https://blog.brandi.codes" },
   { title: "Contact", href: "/contact" },
 ];
